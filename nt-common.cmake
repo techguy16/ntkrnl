c3038c6a98ade3d76148f7bea89852edf1b37ba3
MobSlicer152 <brambleclaw1414@gmail.com>
2022-09-08 08:33:40 -0400

    Fix compile errors

--- nt-common.cmake
+++ nt-common.cmake
@@ -8,8 +8,8 @@ set(CMAKE_C_COMPILER clang-cl)
 set(CMAKE_CXX_COMPILER clang-cl)
 
 set(CLANGWARNINGS "-Wno-microsoft-enum-forward-reference -Wno-visibility -Wno-pragma-pack -Wno-gnu-folding-constant -Wno-inconsistent-dllimport -Wno-ignored-pragma-intrinsic -Wno-parentheses -Wno-implicit-int -Wno-pointer-sign -Wno-microsoft-anon-tag -Wno-ignored-pragma-optimize -Wno-shift-count-negative -Wno-shift-count-overflow")
-set(CMAKE_C_FLAGS_INIT "-fms-compatibility ${CLANGWARNINGS} --target=${CLANGARCH} ${ARCHDEFS}")
-set(CMAKE_CXX_FLAGS_INIT "-fms-compatibility ${CLANGWARNINGS} --target=${CLANGARCH} ${ARCHDEFS}")
+set(CMAKE_C_FLAGS_INIT "-fms-compatibility ${CLANGWARNINGS} --target=${CLANGARCH} ${ARCHDEFS} -Xclang -ffreestanding")
+set(CMAKE_CXX_FLAGS_INIT "-fms-compatibility ${CLANGWARNINGS} --target=${CLANGARCH} ${ARCHDEFS} -Xclang -ffreestanding")
 
 set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_LINKER> -nodefaultlib <LINK_FLAGS> <OBJECTS> <LINK_LIBRARIES>")
 set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_LINKER> -nodefaultlib <LINK_FLAGS> <OBJECTS> <LINK_LIBRARIES>")
